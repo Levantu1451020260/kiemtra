a52d19a02499c5f73170c81a9157d21ef98ea0ef
Lê Văn Tú <90489210+Levantu1451020260@users.noreply.github.com>
2021-10-02 10:46:21 +0700

    Add files via upload

--- baif1.py
+++ baif1.py
@@ -6,5 +6,7 @@ series_ex = {"a":[1, 4, 6, 7, 8], "b":[23, 12, 56, 8, 22], "c":[87, 65, 43, 11, 
 series_data = pd.DataFrame(series_ex)
 
 print(series_data)
-print("series")
+print("in ra cột đầu tiên series")
 print(series_data.iloc[:, 0])
+s (6 sloc)  183 Bytes
+   
